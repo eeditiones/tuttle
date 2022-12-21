553751827b56611c4aa652a1a4db896aa9543b7b
dominik kuehne <domini.kuehne@gmail.com>
2022-12-21 16:20:59 +0100

    testing

--- test/tuttle.js
+++ test/tuttle.js
@@ -9,8 +9,6 @@ describe('Tuttle', function () {
         await util.login()
         response = await util.axios.get('git/status', {})
     })
-//    console.log("DEBUG1: " + JSON.stringify(process.env)); 
-    console.log("DEBUG2: " + process.env.tuttle_token_tuttle_sample_data); 
     it('Status', function () {
         expect(response.status).to.equal(200);
     })
